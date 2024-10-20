513c3e32f0943ba77a18a69b30e982bae3de946b
99narii <122865149+99narii@users.noreply.github.com>
2024-10-20 12:04:31 +0900

    화면 수정

--- src/Pages/Intro/index.tsx
+++ src/Pages/Intro/index.tsx
@@ -8,12 +8,11 @@ export const Intro = () => {
     return (
         <div className='intro'>
             <img className='logo' src='img/Logo.svg' alt='Logo'/>
-            <img className='intro' src='img/intro.png' />
-
+            <div className='intro-img'></div>
             <span className='tit'>
             당신의 성격을 분자로 표현해보세요!
             </span>
-                <button type='button' onClick={() => navigate('/intro')}>시작</button>
+                <button type='button' onClick={() => navigate('/intro')}>시작           </button>
         </div>
     );
 };
